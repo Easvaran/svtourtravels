85e9785ac8b7e449330b89aa2380f6055630b0a9
Easvaran <pmtsandiyargamingboys@gmail.com>
2026-05-11 11:39:58 +0530

    Update sitemap configuration with new options

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,7 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.svtourandtravels.com',
   generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-    additionalSitemaps: ['https://www.svtourandtravels.com/sitemap.xml'],
-  },
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 7000,
 };
