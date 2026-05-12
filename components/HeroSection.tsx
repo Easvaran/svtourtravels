9fa7f1c96a2e59daad030668eccc05410e637e2d
Easvaran <pmtsandiyargamingboys@gmail.com>
2026-05-12 10:52:44 +0530

    Change HeroSection slide transition to slide from right

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -52,10 +52,10 @@ const HeroSection = () => {
       <AnimatePresence mode="wait">
         <motion.div
           key={currentSlide}
-          initial={{ opacity: 0, scale: 1.15 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 1.8 }}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
           className="absolute inset-0 z-0"
         >
           <SafeImage
